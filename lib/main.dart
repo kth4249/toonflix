1e2a95d9bed1bd0efcaf6013c42abe5a8a042bb4
kth4249 <desales4249@gmail.com>
2023-01-16 18:01:09 +0900

    3. ui challenge start

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name;
+
+  Player({this.name});
+}
+
 void main() {
+  var taehun = Player(name: 'taehun');
+
   runApp(App());
 }
 
@@ -9,13 +17,39 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          elevation: 10,
-          centerTitle: true,
-          title: const Text("Hello flutter"),
-        ),
-        body: const Center(
-          child: Text("Hello World"),
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.8), fontSize: 18),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
