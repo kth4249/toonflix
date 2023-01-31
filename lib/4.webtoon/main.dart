9367191334c186c113c6b903ac9260976fe29302
kth4249 <desales4249@gmail.com>
2023-01-31 17:59:47 +0900

    #6 webtoon start

---
+++ lib/4.webtoon/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/4.webtoon/screens/home_screen.dart';
+
+void main(List<String> args) {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
