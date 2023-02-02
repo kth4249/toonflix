be7c182a7d8a240cf3cc4b9f5c6ece8dd085cd84
kth4249 <desales4249@gmail.com>
2023-02-02 16:40:13 +0900

    ~#6.2 Data Fetching

--- lib/4.webtoon/main.dart
+++ lib/4.webtoon/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/4.webtoon/screens/home_screen.dart';
+import 'package:toonflix/4.webtoon/services/api_service.dart';
 
 void main(List<String> args) {
+  ApiService().getTodaysToons();
   runApp(const App());
 }
 
